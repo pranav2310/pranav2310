12bf186cd8a2a427f9d8f754960f9554d29980c3
Pranav Kumar Varshney <89956447+pranav2310@users.noreply.github.com>
2023-01-29 20:51:55 +0530

    Create Convert Time in Hours, Minutes, Seconds to Seconds .py

---
+++ Convert Time in Hours, Minutes, Seconds to Seconds .py
@@ -0,0 +1,8 @@
+#Convert Time in Hours, Minutes, Seconds to Seconds 
+A=int(input("Enter time in Hours"))
+B=int(input("Enter time in Minutes"))
+C=int(input("Enter time in Seconds"))
+D=A*60
+E=(D+B)*60
+S=E+C
+print("time in seconds is",S)
