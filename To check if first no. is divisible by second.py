5071a9e25f9d37b1be0c8b90d9016f11ba1f1cce
Pranav Kumar Varshney <89956447+pranav2310@users.noreply.github.com>
2023-01-29 21:17:29 +0530

    Create To check if first no. is divisible by second.py

---
+++ To check if first no. is divisible by second.py
@@ -0,0 +1,7 @@
+#To check if first no. is divisible by second
+a=int(input("Enter a number"))
+b=int(input("Enter a number"))
+if(a%b==0):
+    print(a," is divisible by ",b)
+else:
+    print("a is not divisible by b")
