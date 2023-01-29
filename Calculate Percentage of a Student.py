79d5d47db2e43d02393fd8786e9bddfe3afe5d19
Pranav Kumar Varshney <89956447+pranav2310@users.noreply.github.com>
2023-01-29 19:45:46 +0530

    Create Calculate Percentage of a Student.py

---
+++ Calculate Percentage of a Student.py
@@ -0,0 +1,8 @@
+#Calculating Percentage of a Student
+m=int(input("Enter Marks in Maths"))
+sc=int(input("Enter Marks in Science"))
+st=int(input("Enter Marks in SSt"))
+e=int(input("Enter Marks in English"))
+L=int(input("Enter Marks in Language"))
+P=(m+sc+st+e+L)/5
+print("Percentage is",P)
